034110b4eef64757fad810b872378f87a2f78f43
Mukesh Bhuriya <67469798+immortusx@users.noreply.github.com>
2022-07-05 09:34:07 +0530

    Secret key debug updates

--- nlive/settings.py
+++ nlive/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d=la_twrdf-5e4j41@bmybd*d^jhgsvd#idfg#)()k6ajj9-1@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["nlive.herokuapp.com"]
 
